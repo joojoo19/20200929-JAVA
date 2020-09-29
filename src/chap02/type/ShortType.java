e92cdaf2870d03abefcf25198d46d3b2e4e1fa31
joojoo19 <juhwange@gmail.com>
2020-09-29 13:54:51 +0900

    datatype

---
+++ src/chap02/type/ShortType.java
@@ -0,0 +1,16 @@
+package chap02.type;
+
+public class ShortType {
+	public static void main(String[] args) {
+		// short : 2byte
+		  
+		short shortValue;
+		shortValue1 = 32767; // 최대값
+		System.out.println(shortValue1);
+		
+		// shortValue1 = 32768; 최대값 초과
+		
+		shortValue1 = -32768; // 최소값
+		System.out.println(shortValue1);
+	}
+}
