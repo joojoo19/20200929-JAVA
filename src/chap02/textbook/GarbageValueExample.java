737aa1ee9964aeaf47bb3c65b0deb7cca055d1f4
joojoo19 <juhwange@gmail.com>
2020-09-29 12:47:32 +0900

    datatype

--- src/chap02/textbook/GarbageValueExample.java
+++ src/chap02/textbook/GarbageValueExample.java
@@ -5,7 +5,7 @@ public class GarbageValueExample {
 		byte var1 = 125;
 		int var2 = 125;
 		
-		for (int i=0; i<25; i++) {
+		for (int i=0; i<25; i++) { // for 중괄호 {} 블록을 5회 반복 실
 			var1++;
 			var2++;
 			System.out.println("var1: " + var1 + "\t" + "var2: " + var2);
