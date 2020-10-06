0ebe21d5e5655b3e9e9f29060187ababbcdfc233
joojoo19 <juhwange@gmail.com>
2020-10-06 09:39:40 +0900

    excercise

--- src/chap03/excercise/Excercise03.java
+++ src/chap03/excercise/Excercise03.java
@@ -4,7 +4,7 @@ public class Excercise03 {
 	public static void main(String[] args) {
 		int score = 85;
 		String result = (!(score>90)) ? "가" : "나";
-		               // !(false) →→  truee
+		               // !(false) → truee
 		System.out.println(result);
 	}
 }
