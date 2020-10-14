6dd2aee597f8795c341a3ce8435c7bbeca97152d
joojoo19 <juhwange@gmail.com>
2020-10-14 11:59:13 +0900

    static

---
+++ src/chap06/lecture/packageKeyword/MyApp.java
@@ -0,0 +1,23 @@
+package chap06.lecture.packageKeyword;
+
+//import java.util.Scanner;
+import java.util.*;  // java.util안에 모든 패키지.
+
+//import java.lang.String; 생략가능
+
+public class MyApp {
+ chap06.lecture.packageKeyword.MyClass o1
+ = new chap06.lecture.packageKeyword.MyClass();
+ 
+ MyClass o2 = new MyClass();
+ 
+ chap06.lecture.MyClass o3 = new chap06.lecture.MyClass();
+ 
+ java.util.Scanner scanner = new java.util.Scanner(System.in);
+ Scanner s2 = new Scanner(System.in);
+ 
+ java.lang.String str = "java";
+ String str2 = "css";
+ 
+ ArrayList list = new ArrayList();
+}
---
+++ src/chap06/textbook/s061204/mycompany/Car.java
@@ -0,0 +1,15 @@
+package chap06.textbook.s061204.mycompany;
+
+import chap06.textbook.s061204.hankok.*;
+import chap06.textbook.s061204.hyundai.Engine;
+import chap06.textbook.s061204.kumho.*;
+
+public class Car {
+	// 필드
+	Engine engine = new Engine();
+	SnowTire tire1 = new SnowTire();
+	BigWidthTire tire2 = new BigWidthTire();
+	chap06.textbook.s061204.hankok.Tire tire3 = new chap06.textbook.s061204.hankok.Tire();
+	chap06.textbook.s061204.kumho.Tire tire4 = new chap06.textbook.s061204.kumho.Tire();
+	
+}
