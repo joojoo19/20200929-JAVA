799ec6d8def92b2817806dfbb4b1c6c0c5bb6b2c
joojoo19 <juhwange@gmail.com>
2020-10-21 16:16:12 +0900

    wrapper

---
+++ src/chap11/wrapper/WrapperEx1.java
@@ -0,0 +1,29 @@
+package chap11.wrapper;
+
+public class WrapperEx1 {
+	public static void main(String[] args) {
+		// byte, short, int, long
+		// float, double, char, boolean
+		
+		// Byte, Short, Int, Long
+		// Float, Double, Char, Boolean
+		
+		int i = 1;
+		
+		Integer in = Integer.valueOf(i);
+		
+		method1(in);
+		
+		int j =2;
+		method1(j);
+		
+		int l = in.intValue();
+		int k = in;
+		
+		Integer in2 = 3;
+		int m = in2;
+	}
+	public static void method1(Object o) {
+		System.out.println(o);
+	}
+}
