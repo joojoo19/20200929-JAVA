6f202610fd9e8efd0245b6800ab5d27c40974ee5
joojoo19 <juhwange@gmail.com>
2020-10-26 11:36:56 +0900

    stack

---
+++ src/chap15/textbook/s150601/StackExample.java
@@ -0,0 +1,21 @@
+package chap15.textbook.s150601;
+
+import java.util.Stack;
+
+public class StackExample {
+	public static void main(String[] args) {
+		Stack<Coin> coinBox = new Stack<Coin>();
+		
+		coinBox.push(new Coin(100));
+		coinBox.push(new Coin(50));
+		coinBox.push(new Coin(500));
+		coinBox.push(new Coin(10));
+		
+		System.out.println(coinBox.get(0).getValue());
+
+		while(!coinBox.isEmpty()) {
+			Coin coin = coinBox.pop();
+			System.out.println("꺼내온 동전 : " + coin.getValue()+"원");
+		}
+	}
+}
